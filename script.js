311a56658ada7c0dbd6b2d7ce7c21d00291130f1
testesdeprojetos <araujo.miguel.trabalho@gmail.com>
2025-07-16 15:51:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -259,6 +259,7 @@ function abrirTrocaVisual() {
 
       // Atualiza e fecha
       ajudanteSelecionado = null;
+      closeModal('trocarModalVisual');
       renderTeams();
       saveToLocalStorage();
     };
